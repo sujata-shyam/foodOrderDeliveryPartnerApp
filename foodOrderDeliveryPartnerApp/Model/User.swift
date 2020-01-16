a5e813e91bbedea3779e5607f66fde5521f4351c
sujata-shyam <sujata.shyam.123@gmail.com>
2020-01-16 10:32:06 +0530

    First Commit

---
+++ foodOrderDeliveryPartnerApp/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  foodOrderDeliveryPartnerApp
+//
+//  Created by Sujata on 15/01/20.
+//  Copyright © 2020 Sujata. All rights reserved.
+//
+
+import Foundation
+
+struct LoginRequest:Codable
+{
+    let id : String?
+}
+
+struct LoginResponse:Codable
+{
+    let msg: String?
+    let session: String?
+    let id: String?
+    let phone: String?
+}
+
+
